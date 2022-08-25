24a6ce1a6d1386d162c44cb7a0ad15e55ccdf86a
siwan <quddlf1302@gmail.com>
2022-08-25 13:19:23 +0900

    schema.sql, data.sql 수정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,43 @@
+
+
+--비행일정 GMP 김포, CJU 제주, ICN 인천, KWJ 광주, USN 울산, MWX 무안, PUS 부산/김해, USN 울산
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('GMP', 'CJU', 'OZ8901', '2022-08-20', '08:10', '20:30');
+
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('GMP', 'CJU', 'OZ7901', '2022-08-24', '06:05', '07:15');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('GMP', 'CJU', '1A7777', '2022-08-24', '07:10', '09:11');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('GMP', 'CJU', '1A7777', '2022-08-24', '09:40', '10:20');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('GMP', 'CJU', 'OZ7901', '2022-08-24', '11:05', '13:15');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('GMP', 'CJU', '5C3300', '2022-08-24', '15:05', '17:15');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('CJU', 'GMP', 'OY8025', '2022-08-14', '18:45', '19:55');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('ICN', 'CJU', '1A7777', '2022-08-30', '11:05', '15:15');
+
+insert into schedule(departure,arrival,planeName,departureDate, departureTime, arrivalTime)
+values ('USN', 'ICN', '5C3300', '2022-09-10', '10:05', '13:15');
+
+
+
+--비행기 종류
+insert into plane(planeName,planeCompany) values ('OZ8901', 'ASIANA');
+insert into plane(planeName,planeCompany) values ('1A7777', 'ASIANA');
+insert into plane(planeName,planeCompany) values ('OY8025', 'ASIANA');
+insert into plane(planeName,planeCompany) values ('5C3300', 'ASIANA');
+
+--유저 더미
+insert into users(userId, userPw, userName, userTel, userBirth)
+values ('siwan','siwan', 'MinSiwan', '01012341234', '830101');
+
