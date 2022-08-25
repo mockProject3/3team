49b31d66682f373d56f5a18bfeb1496ae6153b08
hansh <hansh@asianaidt.com>
2022-08-25 13:25:39 +0900

    sql수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,13 @@
 
---CREATE TABLE schedule(
---       scheduleNum  int NOT NULL AUTO_INCREMENT,
---       planeName   varchar(255) ,
---       departureDate  varchar(255),
---       departureTime  varchar(255)   ,
---       arrivalTime  varchar(255)  ,
---       departure   varchar(255)   ,
---       arrival   varchar(255)  ,
---    PRIMARY KEY(scheduleNum)
---);
-
-CREATE TABLE Plane (
-   planeName   varchar2(30)   NULL,
-   planeCompany   varchar2(30)   NULL
-);
-
-CREATE TABLE Schedule (
-   scheduleNum   integer   NOT NULL AUTO_INCREMENT,
-   planeName   varchar2(30)   NULL,
-   departureDate   varchar2(30)   NULL,
-   departureTime   varchar2(30)   NULL,
-   arrivalTime   varchar2(30)   NULL,
-   departure   varchar2(30)   NULL,
-   arrival   varchar2(30)   NULL
+CREATE TABLE Schedule(
+       scheduleNum  int NOT NULL AUTO_INCREMENT,
+       planeName   varchar(255) ,
+       departureDate  varchar(255),
+       departureTime  varchar(255)   ,
+       arrivalTime  varchar(255)  ,
+       departure   varchar(255)   ,
+       arrival   varchar(255)  ,
+    PRIMARY KEY(scheduleNum)
 );
 
 CREATE TABLE Reservation (
@@ -48,14 +33,6 @@ CREATE TABLE ReservationSeat (
    passNum   integer   NULL,
    ticketNum   integer   NULL,
    scheduleNum   integer   NULL
-);
-
-CREATE TABLE users (
-   userId   varchar(20)   NULL,
-   userPw   varchar(20)   NULL,
-   userName   varchar(10)   NULL,
-   userTel   integer   NULL,
-   userBirth   varchar(20)   NULL
 );
 
 CREATE TABLE Passenger (
