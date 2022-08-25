66d7798618a1193fdca075183914708769d70e46
jusb <jusb@asianaidt.com>
2022-08-25 09:39:30 +0900

    update join data

--- src/main/java/com/example/reservation/User/User.java
+++ src/main/java/com/example/reservation/User/User.java
@@ -15,10 +15,3 @@ public class User {
     private String userTel;
     private String userBirth;
 }
-/*
-* 회원번호 int memberNum
-* 이름 String name
-* 이메일(아이디) String email
-* 연락처 String phone
-* 비밀번호 String pwd
-* */
--- src/main/java/com/example/reservation/User/UserController.java
+++ src/main/java/com/example/reservation/User/UserController.java
@@ -3,6 +3,7 @@ package com.example.reservation.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,16 +21,17 @@ public class UserController {
         return "home";
     }
 
-    @RequestMapping("/register")
-    public String signUp(Model model){
+    @RequestMapping("/join")
+    public String join() {
         return "join";
     }
 
-    @RequestMapping("/complete")
-    public String join(@RequestParam("email") String email, @RequestParam("name") String name,
-                       @RequestParam("phone") String phone, @RequestParam("pwd") String pwd,
-                       @RequestParam("pwd2") String pwd2, Model model) throws Exception {
-        return "join";
+    @PostMapping("/register")
+    public String addUser(@RequestParam("id") String id, @RequestParam("name") String name,
+                          @RequestParam("tel") String tel,@RequestParam("birth") String birth,
+                          @RequestParam("pwd1") String pwd1) {
+        joinService.addUser(id,name,tel,birth,pwd1);
+        return "redirect:/";
     }
 
     @RequestMapping("/login")
