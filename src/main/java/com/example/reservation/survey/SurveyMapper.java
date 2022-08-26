023297cdd4defa64055f6683d139aabb05fe3ed7
jusb <jusb@asianaidt.com>
2022-08-26 09:48:51 +0900

    error fix

--- src/main/java/com/example/reservation/survey/SurveyMapper.java
+++ src/main/java/com/example/reservation/survey/SurveyMapper.java
@@ -2,6 +2,7 @@ package com.example.reservation.survey;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
@@ -11,5 +12,5 @@ public interface SurveyMapper {
     @Insert("INSERT INTO SURVEYS" +
             "(Q1,Q2,Q3,Q4,Q5,Q6,Q7)" +
             "VALUES(#{a1},#{a2},#{a3},#{a4},#{a5},#{a6},#{a7})")
-    void addSurvey(String a1, String a2,String a3,String a4,String a5,String a6, String a7);
+    void addSurvey(@Param("a1") String a1, @Param("a2") String a2, @Param("a3") String a3, @Param("a4") String a4, @Param("a5") String a5, @Param("a6") String a6, @Param("a7") String a7);
 }
